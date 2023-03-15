13630225aa576bdfaf171089955ded185431fb5c
Haqve_vu72 <Haqve_vu72@STHQ0125-07>
2023-03-15 08:50:42 +0400

    Last change => 08:50  =>15.03.2023

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -5,13 +5,9 @@ using namespace std;
 #include "Car.h"
 void main()
 {
-	map<Car, Fine>DYP;
+	map<Car , Fine>DYP;
 	Car c1("Toyota", "Prius", 1990 );
 	Fine f1(120, "03/03/2020" );
-
-	Car c2 ("Toyota", "Prius", 1994 );
-	Fine f2 (100, "03/03/2021" );
-
-	Fine temp(DYP[c2] = f2);
+	Fine temp(DYP[c1] = f1);
 	temp.show();
 }
